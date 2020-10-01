064f287c97d2d8838ec4c256df212677bafbd0b3
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-10-01 22:46:06 +0900

    if expression

--- rust-official-guide/chap3-Common-Programming-Concepts/src/main.rs
+++ rust-official-guide/chap3-Common-Programming-Concepts/src/main.rs
@@ -1,11 +1,11 @@
-// five 関数はRustにおいて、完璧に問題ない関数。
-// セミコロンが無い部分が戻り値の式となる。
-fn five() -> i32 {
-    5
-}
-
 fn main() {
-    let x = five();
+    let condition = true;
+    let number = if condition {
+        5
+    } else {
+        6
+    };
 
-    println!("The value of x is: {}", x);
+    // numberの値は、{}です
+    println!("The value of number is: {}", number);
 }
