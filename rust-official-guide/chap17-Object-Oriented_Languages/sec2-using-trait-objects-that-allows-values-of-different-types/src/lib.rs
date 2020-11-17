dd8c9a1c084a5bd4bb6c829126f714fcbf9900dd
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2020-11-17 23:58:50 +0900

    update comment

--- rust-official-guide/chap17-Object-Oriented_Languages/sec2-using-trait-objects-that-allows-values-of-different-types/src/lib.rs
+++ rust-official-guide/chap17-Object-Oriented_Languages/sec2-using-trait-objects-that-allows-values-of-different-types/src/lib.rs
@@ -5,8 +5,10 @@ pub trait Draw {
 // trait object (`Box<dyn Draw>`) を持ったScreen構造体
 pub struct Screen {
     pub components: Vec<Box<dyn Draw>>, // インタフェースを満たすTrait Objectを持つベクター
-    // Vec<T>で where T: Draw
-    // ではインタフェースを満たす一種類の型しかVecの中の値に入れることができない。
+    // [VS] Vec<T>で where T: Draw ← Using generics and Trait Bounds
+    // 上記はインタフェースを満たす一種類の型しかVecの中の値に入れることができない。
+    // Trait Object ← Dynamic Dispatch: 実行時にどの具象メソッドかを見に行くので↓より遅い
+    // Using generics and Trait Bounds ← Static Dispatch: コンパイル時にどのメソッドかがわかり速い
 }
 
 impl Screen {
