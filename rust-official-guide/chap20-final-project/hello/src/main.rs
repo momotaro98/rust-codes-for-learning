741fa08edf8a8daf18d033bef82ec67218b8e237
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2021-01-05 23:01:35 +0900

    無限スレッドを作れるのでDDOS攻撃されてしまう状態

--- rust-official-guide/chap20-final-project/hello/src/main.rs
+++ rust-official-guide/chap20-final-project/hello/src/main.rs
@@ -2,6 +2,8 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -9,7 +11,9 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        thread::spawn(|| { // TODO: 無限スレッド作れるのでDDOS攻撃でリソースを使い果たしてしまう
+            handle_connection(stream);
+        });
     }
 }
 
@@ -18,8 +22,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
