15058f798deb1de12f47137ca5bdfb6ec58af29c
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2021-01-07 00:04:34 +0900

    update

--- rust-official-guide/chap20-final-project/hello/src/lib.rs
+++ rust-official-guide/chap20-final-project/hello/src/lib.rs
@@ -1,11 +1,21 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
         // if size <= 0 then got > thread 'main' panicked at 'assertion failed: size > 0', src/lib.rs:5:9
 
-        ThreadPool
+        let mut threads = Vec::with_capacity(size);
+
+        for _ in 0..size {
+            // create some threads and store them in the vector
+        }
+
+        ThreadPool { threads }
     }
 
     pub fn execute<F>(&self, f: F)
