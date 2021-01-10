0125d6a9375dc8e9dbe5a2a713b0ff7b858bc6f4
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2021-01-11 00:50:33 +0900

    finish 20.3 and Rust the book

--- rust-official-guide/chap20-final-project/hello/README.md
+++ rust-official-guide/chap20-final-project/hello/README.md
@@ -20,3 +20,33 @@ impl Worker {
 > This code compiles and runs but doesn’t result in the desired threading behavior: a slow request will still cause other requests to wait to be processed. The reason is somewhat subtle: the Mutex struct has no public unlock method because the ownership of the lock is based on the lifetime of the MutexGuard<T> within the LockResult<MutexGuard<T>> that the lock method returns. At compile time, the borrow checker can then enforce the rule that a resource guarded by a Mutex cannot be accessed unless we hold the lock. But this implementation can also result in the lock being held longer than intended if we don’t think carefully about the lifetime of the MutexGuard<T>. Because the values in the while let expression remain in scope for the duration of the block, the lock remains held for the duration of the call to job(), meaning other workers cannot receive jobs.
 
 > By using loop instead and acquiring the lock without assigning to a variable, the temporary MutexGuard returned from the lock method is dropped as soon as the let job statement ends. This ensures that the lock is held during the call to recv, but it is released before the call to job(), allowing multiple requests to be serviced concurrently.
+
+# Run
+
+```
+$ cargo run
+```
+
+```
+$ curl http://localhost:/sleep
+$ curl http://localhost:/
+```
+
+then
+
+```
+    Finished dev [unoptimized + debuginfo] target(s) in 0.00s
+     Running `target/debug/main`
+Worker 1 got a job; executing.
+Sending terminate message to all workers.
+Shutting down all workers.
+Shutting down worker 0
+Worker 0 got a job; executing.
+Worker 2 was told to terminate.
+Worker 3 was told to terminate.
+Worker 1 was told to terminate.
+Worker 0 was told to terminate.
+Shutting down worker 1
+Shutting down worker 2
+Shutting down worker 3
+```
--- rust-official-guide/chap20-final-project/hello/src/lib.rs
+++ rust-official-guide/chap20-final-project/hello/src/lib.rs
@@ -14,13 +14,22 @@ pub struct Worker {
 // > we need to move the thread out of the Worker instance that owns thread so join can consume the thread. We did this in Listing 17-15: if Worker holds an Option<thread::JoinHandle<()>> instead, we can call the take method on the Option to move the value out of the Some variant and leave a None variant in its place. In other words, a Worker that is running will have a Some variant in thread, and when we want to clean up a Worker, we’ll replace Some with None so the Worker doesn’t have a thread to run.
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
 
-            println!("Worker {} got a job; executing.", id);
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
 
-            job();
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+
+                    break;
+                }
+            }
         });
 
         Worker {
@@ -32,9 +41,14 @@ impl Worker {
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 impl ThreadPool {
@@ -61,12 +75,24 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        // [Note] self.sender.send と thread.join() のfor loop
+        // を1つにしようとすると self.sender.send が非同期であるために、デッドロックになってしまう。
+        // [原文] > To better understand why we need two separate loops, imagine a scenario with two workers. If we used a single loop to iterate through each worker, on the first iteration a terminate message would be sent down the channel and join called on the first worker’s thread. If that first worker was busy processing a request at that moment, the second worker would pick up the terminate message from the channel and shut down. We would be left waiting on the first worker to shut down, but it never would because the second thread picked up the terminate message. Deadlock!
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
